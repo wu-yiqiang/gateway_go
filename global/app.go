7caffa3f85d4994f56973fb3fdfe75e433578982
wu-yiqiang <wu_yiqiang@outlook.com>
2023-03-11 21:22:25 +0800

    数据库初始化

--- global/app.go
+++ global/app.go
@@ -4,12 +4,14 @@ import (
   "github.com/spf13/viper"
   "gateway_go/config"
   "go.uber.org/zap"
+  "gorm.io/gorm"
 )
 
 type Application struct {
   ConfigViper *viper.Viper
   Config config.Configuration
   Log *zap.Logger
+  DB *gorm.DB
 }
 
 var App = new(Application)
